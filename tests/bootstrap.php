2754384c0c4e0b389effa005f522802f40885610
Bakin Vlad <mixkorshun@gmail.com>
2014-07-24 23:55:35 +0400

    Add composer autoloader register in tests bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,9 @@
 <?php
+/**
+ * @var \Composer\Autoload\ClassLoader $autoload
+ */
+$autoload = require dirname(__DIR__) . '/vendor/autoload.php';
 
 define('TEMP_PATH', __DIR__ . '/EazyTest/Temp');
+
+$autoload->add('EazyTest', array(__DIR__));
